c8b4c1dcb145263a39707fc095ff92548f575046
Eleanor Thomas <eleanor.thomas.res@gmail.com>
2017-06-03 03:01:28 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -9,11 +9,11 @@ DBNAME = "tvshows"
 
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].sort.each {|f| require f}
 
 connection_details = YAML::load(File.open('config/database.yml'))
 
-# DBRegistry[ENV["ACTIVE_RECORD_ENV"]].connect!
+#DBRegistry[ENV["ACTIVE_RECORD_ENV"]].connect!
 DB = ActiveRecord::Base.establish_connection(connection_details)
 
 if ENV["ACTIVE_RECORD_ENV"] == "test"
